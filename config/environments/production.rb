cb312c091a64e30513bdcbfbe5a7dec90df56d4c
yonatan <yonatnh@gmail.com>
2021-03-31 11:33:32 +0300

    MH tutorial bootstrap setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "studio_app_production"
+  # config.active_job.queue_name_prefix = "the_studio_production"
 
   config.action_mailer.perform_caching = false
 
@@ -109,4 +109,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+  config.action_mailer.default_url_options = { :host => 'the-studio-bll.co.il' }
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_user'],
+    :password             => ENV['gmail_pass'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
 end
