6c84930edc6fb8d2e0b96b3051d4ace86e8e140b
yonatan <yonatnh@gmail.com>
2021-04-27 22:28:28 +0300

    mailer fuckup fixed

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,6 +3,6 @@ class AdminMailer < ApplicationMailer
   def new_contact(contact)
     @contact = contact
 
-    mail to: ENV['admin1_mail'], ENV['admin2_mail'], ENV['admin3_mail']], subject: "New Contact"
+    mail to: [ENV['admin1_mail'], ENV['admin2_mail'], ENV['admin3_mail']], subject: "New Contact"
   end
 end
