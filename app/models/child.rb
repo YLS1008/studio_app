50029428cc2cbeb8f129eb9cf6236420bb42f58c
yonatan <yonatnh@gmail.com>
2021-05-10 23:24:29 +0300

    fixed child deletion bug

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,6 +1,6 @@
 class Child < ApplicationRecord
   belongs_to :trainee
-
+  before_destroy :destroy_child_trainee
   def self.is_child?(id)
     @parent = Child.where(child_trainee_id: id)
     if @parent.empty?
@@ -14,4 +14,10 @@ class Child < ApplicationRecord
     @parent = Child.where(child_trainee_id: id).first
     return Trainee.find(@parent.trainee_id)
   end
+
+  private
+  def destroy_child_trainee
+    Trainee.where(id: self.child_trainee_id).destroy
+  end
+  
 end
